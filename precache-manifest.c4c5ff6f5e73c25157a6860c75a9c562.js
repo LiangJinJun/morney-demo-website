a4a6a4a4149a29670c6b6f3fe97e47f0fc399ef6
LiangJinJun <2210258654@qq.com>
2020-12-24 18:05:59 +0800

    deploy

---
+++ precache-manifest.c4c5ff6f5e73c25157a6860c75a9c562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eddf08952f9b0a1238b",
+    "url": "/morney-demo-website/css/app.1811c08d.css"
+  },
+  {
+    "revision": "da616fecce21398e73caa4e5e0f9bea3",
+    "url": "/morney-demo-website/index.html"
+  },
+  {
+    "revision": "6eddf08952f9b0a1238b",
+    "url": "/morney-demo-website/js/app.881de49b.js"
+  },
+  {
+    "revision": "82886c43c00606477010",
+    "url": "/morney-demo-website/js/chunk-vendors.4d2d9b83.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/morney-demo-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-demo-website/robots.txt"
+  }
+]);
