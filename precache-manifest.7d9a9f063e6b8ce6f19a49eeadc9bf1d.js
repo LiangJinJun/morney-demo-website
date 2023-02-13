54f139edff4bb19e9d4e35b9a8ae75b660b253aa
liangjinjun <2210258654@qq.com>
2023-02-13 17:32:04 +0800

    deploy

---
+++ precache-manifest.7d9a9f063e6b8ce6f19a49eeadc9bf1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8a8b27cb677a8abf51",
+    "url": "/morney-demo-website/css/app.96a5b2e0.css"
+  },
+  {
+    "revision": "143fcce4ed8522900d7c94468dc5bc61",
+    "url": "/morney-demo-website/index.html"
+  },
+  {
+    "revision": "7e8a8b27cb677a8abf51",
+    "url": "/morney-demo-website/js/app.18dcd9dc.js"
+  },
+  {
+    "revision": "77de356ea3c702691667",
+    "url": "/morney-demo-website/js/chunk-vendors.e2b3a6cf.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/morney-demo-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-demo-website/robots.txt"
+  }
+]);
